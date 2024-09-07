eb3f4262704939622c5631f854d4a903f6473e95
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-09-07 18:10:35 +0200

    added 200 OK response

--- app/server.go
+++ app/server.go
@@ -7,20 +7,29 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-
+	// Steps to build an http server:
+	// First, a listener is created and bound to a tcp port. In this case is port 4221
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close() // listener closure is deferred to ensure it's closed after program exits
 
-	_, err = l.Accept()
+	// Second, we tell the listener to accept incoming connections
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close() // connection closure is deferred to ensure it's closed after program exits
+
+	// Third, we respond to the incoming connection, confirming the success in this case
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+	}
+
 }
