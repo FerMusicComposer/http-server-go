b0443762daf0d409b584104a30d3ede216966b7b
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-09-20 22:36:14 +0200

    retrying extract url stage test

--- app/server.go
+++ app/server.go
@@ -49,7 +49,7 @@ func main() {
 	if path == "/" {
 		response = "HTTP/1.1 200 OK\r\n\r\n"
 	} else {
-		response = "HTTP/1.1 404 Not found\r\n\r\n"
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
 
 	_, err = conn.Write([]byte(response))
